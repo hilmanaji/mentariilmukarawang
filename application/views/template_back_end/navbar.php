04bc727c990567755f2c920f8dfb5f3b73c96d67
Fazri Ramadhan <fazri.rramadhanh@gmail.com>
2019-08-03 20:47:05 +0700

    minor

--- application/views/template_back_end/navbar.php
+++ application/views/template_back_end/navbar.php
@@ -17,12 +17,12 @@
                     <li>
                         <a href="javascript:void(0)" class="sidebartoggler font-20 waves-effect waves-light"><i class="icon-arrow-left-circle"></i></a>
                     </li>
-                    <!-- <li>
+                    <li>
                         <form role="search" class="app-search hidden-xs">
                             <i class="icon-magnifier"></i>
-                            <input type="text" placeholder="Search..." class="form-control">
+                            <input type="text" placeholder="Cari..." class="form-control">
                         </form>
-                    </li> -->
+                    </li>
                 </ul>
                 <ul class="nav navbar-top-links navbar-right pull-right">
                     <li class="right-side-toggle">
