168e7498017ecceda00299a900f3f26f94e75c44
Fazri Ramadhan <fazri.rramadhanh@gmail.com>
2019-08-19 11:42:58 +0700

    rev minor
    minor

---
+++ application/controllers/Ekskul.php
@@ -0,0 +1,267 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Ekskul extends CI_Controller {
+
+    private $role_user, $id_sekolah, $kondisi;
+    private $nama_tabel = 'tbl_ekskul';
+
+    function __construct() {
+        parent::__construct();
+        if (!$this->session->has_userdata('id_user')) {
+            redirect('Login');
+        }
+        else{            
+            $id_user = $this->session->userdata('id_user');
+            $this->role_user = $this->session->userdata('role_user');
+            $this->id_sekolah = $this->session->userdata('id_sekolah');
+        }        
+        if ($this->role_user === '2') { 
+            $kondisi = "AND ".$this->nama_tabel.".id_sekolah = '".$this->id_sekolah."'";
+            $this->kondisi = $kondisi;
+        }
+        else{
+            $this->kondisi = '';
+        }
+    }
+
+	public function index()
+	{
+        $data['id_sekolah'] = $this->id_sekolah;
+        $data['data_sekolah'] = $this->DataHandle->getAllWhere('tbl_sekolah', '*', "status = '1'");          
+        $data['data_ekskul'] = $this->DataHandle->get_two($this->nama_tabel, 'tbl_sekolah',''.$this->nama_tabel.'.*, tbl_sekolah.nama as nama_sekolah', 'tbl_sekolah.id_sekolah = '.$this->nama_tabel.'.id_sekolah', "".$this->nama_tabel.".status = '1' ".$this->kondisi."");		
+        $this->template->back_end('back_end/v_data_ekskul', $data);
+    }
+
+    public function form_add()
+    {
+        $data['id_sekolah'] = $this->id_sekolah;
+        $data['data_sekolah'] = $this->DataHandle->getAllWhere('tbl_sekolah', '*', "status = '1'");
+        $this->template->back_end('back_end/v_add_ekskul', $data);
+    }
+
+    public function add(){
+        $this->load->library('upload'); //pemanggilan library upload
+        $id_user = $this->session->userdata('id_user');
+        $id_sekolah = $this->input->post('id_sekolah');
+        $nama_ekskul = $this->input->post('nama_ekskul');
+        $deskripsi_ekskul = $this->input->post('deskripsi_ekskul');
+
+        // KONDISI GAMBAR ADA
+        if ($_FILES['userfile']['name'] != NULL) {
+            $dataInfo = array();
+            $files = $_FILES;
+            $_FILES['userfile']['name']= $files['userfile']['name'];
+            $_FILES['userfile']['type']= $files['userfile']['type'];
+            $_FILES['userfile']['tmp_name']= $files['userfile']['tmp_name'];
+            $_FILES['userfile']['error']= $files['userfile']['error'];
+            $_FILES['userfile']['size']= $files['userfile']['size']; 
+
+            $this->upload->initialize($this->set_upload_options());
+            if ($this->upload->do_upload()) {
+                $dataInfo = $this->upload->data();         
+                // DATA INPUT EKSKUL
+                $inpute_ekskul = array(
+                    'id_sekolah' => $id_sekolah,
+                    'nama_ekskul' => $nama_ekskul,
+                    'deskripsi_ekskul' => $deskripsi_ekskul,
+                    'value' => $dataInfo['file_name'],
+                    'status' => 1,
+                    'created_by' => $id_user
+                 );
+                $this->DataHandle->insert($this->nama_tabel, $inpute_ekskul);    
+                $this->session->set_flashdata('msg', '
+                <div class="alert alert-success alert-dismissable">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
+                    &times;</button>
+                    <i class="fa fa-check m-l-5"></i> Data Berhasil Ditambahkan ... 
+                </div>');  
+                redirect('Ekskul');   
+
+            }
+            else{
+                $this->session->set_flashdata('msg', '
+                <div class="alert alert-danger alert-dismissable">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
+                    &times;</button>
+                    <i class="fa fa-check m-l-5"></i> Gambar Bermasalah !!!!
+                </div>');  
+
+                redirect('Ekskul');
+            }
+            
+        }
+        // KONDISI GAMBAR KOSONG
+        else{ 
+            // DATA INPUT EKSKUL
+            $input_ekskul = array(
+                'id_sekolah' => $id_sekolah,
+                'nama_ekskul' => $nama_ekskul,
+                'deskripsi_ekskul' => $deskripsi_ekskul,
+                'status' => 1,
+                'created_by' => $id_user
+             );
+            $this->DataHandle->insert($this->nama_tabel, $input_ekskul);  
+            $this->session->set_flashdata('msg', '
+            <div class="alert alert-success alert-dismissable">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
+                &times;</button>
+                <i class="fa fa-check m-l-5"></i> Data Berhasil Ditambahkan ... 
+            </div>');  
+            redirect('Ekskul');   
+        }      
+    }
+
+    public function delete($id_ekskul)
+    {
+        $id_user = $this->session->userdata('id_user');
+        $data = array(
+            'status' => '0',
+            'updated_by' => $id_user
+         );
+        $where = array(
+            'id_ekskul' => $id_ekskul
+         );
+        $this->DataHandle->edit($this->nama_tabel, $data, $where);
+
+        $this->session->set_flashdata('msg', '
+        <div class="alert alert-warning alert-dismissable">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
+            &times;</button>
+            <i class="fa fa-check m-l-5"></i> Data Berhasil Dihapus ... 
+        </div>');  
+
+        redirect('Ekskul');     
+    }
+
+    public function get_data($id_ekskul){
+        $data['id_sekolah_sess'] = $this->id_sekolah;
+        $data['data_sekolah'] = $this->DataHandle->getAllWhere('tbl_sekolah', '*', "status = '1'"); 
+        $where = array(
+            'id_ekskul' => $id_ekskul
+         );
+        $data['data_ekskul'] = $this->DataHandle->getAllWhere($this->nama_tabel, '*', $where);
+        $this->template->back_end('back_end/v_edit_ekskul', $data);
+    }
+
+    public function edit(){
+        $this->load->library('upload'); //pemanggilan library upload
+        $id_user = $this->session->userdata('id_user');
+        $id_sekolah = $this->input->post('id_sekolah');
+        $id_ekskul = $this->input->post('id_ekskul'); 
+        $deskripsi_ekskul = $this->input->post('deskripsi_ekskul');
+        $nama_ekskul = $this->input->post('nama_ekskul');
+
+        // KONDISI GAMBAR ADA
+        if ($_FILES['userfile']['name'] != NULL) {
+            $gambar_lama = $this->input->post('gambar_lama');
+            $dataInfo = array();
+            $files = $_FILES;
+            $_FILES['userfile']['name']= $files['userfile']['name'];
+            $_FILES['userfile']['type']= $files['userfile']['type'];
+            $_FILES['userfile']['tmp_name']= $files['userfile']['tmp_name'];
+            $_FILES['userfile']['error']= $files['userfile']['error'];
+            $_FILES['userfile']['size']= $files['userfile']['size']; 
+
+            $this->upload->initialize($this->set_upload_options());
+            if ($this->upload->do_upload()) {
+                $dataInfo = $this->upload->data();         
+                // DATA EDIT ARTIKEL
+                $edit_data = array(
+                    'id_sekolah' => $id_sekolah,
+                    'deskripsi_ekskul' => $deskripsi_ekskul,
+                    'nama_ekskul' => $nama_ekskul,
+                    'updated_by' => $id_user
+                 );
+                $where = array(
+                    'id_ekskul' => $id_ekskul
+                 );
+                $this->DataHandle->edit($this->nama_tabel, $edit_data, $where);    
+
+                // JIKA GAMBAR TIDAK ADA
+                if ($gambar_lama == "") {
+                    // DATA EDIT ARTIKEL
+                    $edit_data = array(
+                        'id_sekolah' => $id_sekolah,
+                        'deskripsi_ekskul' => $deskripsi_ekskul,
+                        'value' => $dataInfo['file_name'],
+                        'nama_ekskul' => $nama_ekskul,
+                        'updated_by' => $id_user
+                     );
+                    $where = array(
+                        'id_ekskul' => $id_ekskul
+                     );
+                    $this->DataHandle->edit($this->nama_tabel, $edit_data, $where);
+                }
+                // JIKA GAMBAR ADA
+                else{            // DATA EDIT ARTIKEL
+                    $edit_data = array(
+                        'id_sekolah' => $id_sekolah,
+                        'deskripsi_ekskul' => $deskripsi_ekskul,
+                        'nama_ekskul' => $nama_ekskul,
+                        'value' => $dataInfo['file_name'],
+                        'updated_by' => $id_user
+                     );
+                    $where = array(
+                        'id_ekskul' => $id_ekskul
+                     );
+                    $this->DataHandle->edit($this->nama_tabel, $edit_data, $where);
+                    unlink('./assets/plugins/images/image/'.$gambar_lama);
+                }   
+                // HAPUS GAMBAR LAMA   
+                $this->session->set_flashdata('msg', '
+                <div class="alert alert-success alert-dismissable">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
+                    &times;</button>
+                <i class="fa fa-check m-l-5"></i> Data Berhasil Perbaharui ... 
+                </div>');  
+                redirect('Ekskul');   
+
+            }
+            else{
+                $this->session->set_flashdata('msg', '
+                <div class="alert alert-danger alert-dismissable">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
+                    &times;</button>
+                    <i class="fa fa-check m-l-5"></i> Gambar Bermasalah !!!!
+                </div>');  
+
+                redirect('Ekskul');
+            }
+            
+        }
+        // KONDISI GAMBAR KOSONG
+        else{
+            // DATA EDIT ARTIKEL
+            $edit_data = array(
+                'id_sekolah' => $id_sekolah,
+                'deskripsi_ekskul' => $deskripsi_ekskul,
+                'nama_ekskul' => $nama_ekskul,
+                'updated_by' => $id_user
+             );
+            $where = array(
+                'id_ekskul' => $id_ekskul
+             );
+            $this->DataHandle->edit($this->nama_tabel, $edit_data, $where);
+            $this->session->set_flashdata('msg', '
+            <div class="alert alert-success alert-dismissable">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
+                &times;</button>
+                <i class="fa fa-check m-l-5"></i> Data Berhasil Perbaharui ... 
+            </div>');  
+            redirect('Ekskul');   
+        }
+    }
+
+    private function set_upload_options(){
+        $config['upload_path'] = './assets/plugins/images/image';
+        $config['allowed_types'] = 'gif|jpg|png|jpeg|pdf';
+        $config['max_size'] = 2048;
+        $config['max_width'] = 1366;
+        $config['max_height'] = 1368;
+        $config['encrypt_name'] = TRUE;
+        $config['overwrite']     = FALSE;
+
+        return $config;        
+    }
+}
--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,14 +3,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
-	// =============================== TEMPLATE LAMA ==============================
-	// public function index()
-	// {
-	// 	$data['data_sekolah'] = $this->DataHandle->getAllWhere('tbl_sekolah', '*', "status = '1'");
-	// 	$data['data_artikel'] = $this->DataHandle->get2lim6('tbl_artikel', 'tbl_user', 'tbl_artikel.*, tbl_user.username', 'tbl_artikel.created_by = tbl_user.id_user', "tbl_artikel.status = '1'", "tbl_artikel.id_artikel");
-	// 	$this->template->front_end('front_end/v_home', $data);
-	// }
-
 	public function welcome()
 	{
 		$this->template->front_end('front_end/v_welcome');
