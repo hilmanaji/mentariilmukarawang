9ae5497181eb61cf4c3e4c2c5abb9ec27e719ab2
ountaarab <52645686+ountaarab@users.noreply.github.com>
2020-01-30 14:55:55 +0700

    Rev Minor

--- application/controllers/Artikel.php
+++ application/controllers/Artikel.php
@@ -11,12 +11,12 @@ class Artikel extends CI_Controller {
         if (!$this->session->has_userdata('id_user')) {
             redirect('Login');
         }
-        else{            
+        else{
             $id_user = $this->session->userdata('id_user');
             $this->role_user = $this->session->userdata('role_user');
             $this->id_sekolah = $this->session->userdata('id_sekolah');
-        }        
-        if ($this->role_user === '2') { 
+        }
+        if ($this->role_user === '2') {
             $kondisi = "AND ".$this->nama_tabel.".id_sekolah = '".$this->id_sekolah."'";
             $this->kondisi = $kondisi;
         }
@@ -28,7 +28,7 @@ class Artikel extends CI_Controller {
     public function index()
     {
         $data['data_sekolah'] = $this->DataHandle->getAllWhere('tbl_sekolah', '*', "status = '1' AND id_sekolah != '0'");
-        $artikel = $this->DataHandle->other_query("SELECT tbl_sekolah.nama as nama_sekolah, tbl_sekolah.id_sekolah, ".$this->nama_tabel.".* FROM ".$this->nama_tabel." INNER JOIN tbl_sekolah ON ".$this->nama_tabel.".id_sekolah = tbl_sekolah.id_sekolah WHERE ".$this->nama_tabel.".`status` = '1' ".$this->kondisi."")->result_array();  
+        $artikel = $this->DataHandle->other_query("SELECT tbl_sekolah.nama as nama_sekolah, tbl_sekolah.id_sekolah, ".$this->nama_tabel.".* FROM ".$this->nama_tabel." INNER JOIN tbl_sekolah ON ".$this->nama_tabel.".id_sekolah = tbl_sekolah.id_sekolah WHERE ".$this->nama_tabel.".`status` = '1' ".$this->kondisi."")->result_array();
         $data['data_artikel'] = $artikel;
 
         // var_dump(count($data['data_artikel']));die;
@@ -59,15 +59,15 @@ class Artikel extends CI_Controller {
             $_FILES['userfile']['type']= $files['userfile']['type'];
             $_FILES['userfile']['tmp_name']= $files['userfile']['tmp_name'];
             $_FILES['userfile']['error']= $files['userfile']['error'];
-            $_FILES['userfile']['size']= $files['userfile']['size']; 
+            $_FILES['userfile']['size']= $files['userfile']['size'];
 
             $this->upload->initialize($this->set_upload_options());
             if ($this->upload->do_upload()) {
-                $dataInfo = $this->upload->data(); 
+                $dataInfo = $this->upload->data();
                 if ($dataInfo['file_size'] > 1024) {
                     $this->resize($dataInfo['file_name']);
 
-                }                    
+                }
                 // DATA INPUT ARTIKEL
                 $input_artikel = array(
                     'id_sekolah' => $id_sekolah,
@@ -76,28 +76,28 @@ class Artikel extends CI_Controller {
                     'judul_artikel' => $judul_artikel,
                     'status' => 1,
                     'created_by' => $id_user
-                 );
-                $this->DataHandle->insert('tbl_artikel', $input_artikel);    
+                );
+                $this->DataHandle->insert('tbl_artikel', $input_artikel);
                 $this->session->set_flashdata('msg', '
-                <div class="alert alert-success alert-dismissable">
+                    <div class="alert alert-success alert-dismissable">
                     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
                     &times;</button>
-                    <i class="fa fa-check m-l-5"></i> Data Berhasil Ditambahkan ... 
-                </div>');  
-                redirect('Artikel');   
+                    <i class="fa fa-check m-l-5"></i> Data Berhasil Ditambahkan ...
+                    </div>');
+                redirect('Artikel');
 
             }
             else{
                 $this->session->set_flashdata('msg', '
-                <div class="alert alert-danger alert-dismissable">
+                    <div class="alert alert-danger alert-dismissable">
                     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
                     &times;</button>
                     <i class="fa fa-check m-l-5"></i> Gambar Bermasalah !!!!
-                </div>');  
+                    </div>');
 
                 redirect('Artikel');
             }
-            
+
         }
         // KONDISI GAMBAR KOSONG
         else{
@@ -108,16 +108,16 @@ class Artikel extends CI_Controller {
                 'judul_artikel' => $judul_artikel,
                 'status' => 1,
                 'created_by' => $id_user
-             );
-            $this->DataHandle->insert('tbl_artikel', $input_artikel);  
+            );
+            $this->DataHandle->insert('tbl_artikel', $input_artikel);
             $this->session->set_flashdata('msg', '
-            <div class="alert alert-success alert-dismissable">
+                <div class="alert alert-success alert-dismissable">
                 <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
                 &times;</button>
-                <i class="fa fa-check m-l-5"></i> Data Berhasil Ditambahkan ... 
-            </div>');  
-            redirect('Artikel');   
-        }    
+                <i class="fa fa-check m-l-5"></i> Data Berhasil Ditambahkan ...
+                </div>');
+            redirect('Artikel');
+        }
     }
 
     public function delete($id_artikel)
@@ -126,33 +126,33 @@ class Artikel extends CI_Controller {
         $data = array(
             'status' => '0',
             'updated_by' => $id_user
-         );
+        );
         $where = array(
             'id_artikel' => $id_artikel
-         );
+        );
         $this->DataHandle->edit('tbl_artikel', $data, $where);
 
         $this->session->set_flashdata('msg', '
-        <div class="alert alert-warning alert-dismissable">
+            <div class="alert alert-warning alert-dismissable">
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
             &times;</button>
-            <i class="fa fa-check m-l-5"></i> Data Berhasil Dihapus ... 
-        </div>');  
+            <i class="fa fa-check m-l-5"></i> Data Berhasil Dihapus ...
+            </div>');
 
-        redirect('Artikel');     
+        redirect('Artikel');
     }
 
     public function get_data($id_artikel, $id_sekolah = null){
         $data['id_sekolah_sess'] = $this->id_sekolah;
-        $data['data_sekolah'] = $this->DataHandle->getAllWhere('tbl_sekolah', '*', "status = '1' AND id_sekolah != '0'"); 
+        $data['data_sekolah'] = $this->DataHandle->getAllWhere('tbl_sekolah', '*', "status = '1' AND id_sekolah != '0'");
         $where = array(
             'id_artikel' => $id_artikel
-         );
+        );
         if($id_sekolah != '0'){
-            $data['data_artikel'] = $this->DataHandle->other_query("SELECT tbl_sekolah.nama as nama_sekolah, tbl_sekolah.id_sekolah, tbl_artikel.id_artikel, tbl_artikel.judul_artikel, tbl_artikel.isi, tbl_artikel.`status`, tbl_artikel.`value` FROM tbl_artikel INNER JOIN tbl_sekolah ON tbl_artikel.id_sekolah = tbl_sekolah.id_sekolah WHERE tbl_artikel.`status` = '1' AND tbl_artikel.`id_artikel` = '".$id_artikel."'");            
+            $data['data_artikel'] = $this->DataHandle->other_query("SELECT tbl_sekolah.nama as nama_sekolah, tbl_sekolah.id_sekolah, tbl_artikel.id_artikel, tbl_artikel.judul_artikel, tbl_artikel.isi, tbl_artikel.`status`, tbl_artikel.`value` FROM tbl_artikel INNER JOIN tbl_sekolah ON tbl_artikel.id_sekolah = tbl_sekolah.id_sekolah WHERE tbl_artikel.`status` = '1' AND tbl_artikel.`id_artikel` = '".$id_artikel."'");
         }
         else{
-            $data['data_artikel'] = $this->DataHandle->other_query("SELECT tbl_sekolah.nama as nama_sekolah, tbl_sekolah.id_sekolah, tbl_artikel.id_artikel, tbl_artikel.judul_artikel, tbl_artikel.isi, tbl_artikel.`status`, tbl_artikel.`value` FROM tbl_artikel LEFT JOIN tbl_sekolah ON tbl_artikel.id_sekolah = tbl_sekolah.id_sekolah WHERE tbl_artikel.`status` = '1' AND tbl_artikel.`id_artikel` = '".$id_artikel."'");            
+            $data['data_artikel'] = $this->DataHandle->other_query("SELECT tbl_sekolah.nama as nama_sekolah, tbl_sekolah.id_sekolah, tbl_artikel.id_artikel, tbl_artikel.judul_artikel, tbl_artikel.isi, tbl_artikel.`status`, tbl_artikel.`value` FROM tbl_artikel LEFT JOIN tbl_sekolah ON tbl_artikel.id_sekolah = tbl_sekolah.id_sekolah WHERE tbl_artikel.`status` = '1' AND tbl_artikel.`id_artikel` = '".$id_artikel."'");
         }
         $this->template->back_end('back_end/v_edit_artikel', $data);
     }
@@ -161,7 +161,7 @@ class Artikel extends CI_Controller {
         $this->load->library('upload'); //pemanggilan library upload
         $id_user = $this->session->userdata('id_user');
         $id_sekolah = $this->input->post('id_sekolah');
-        $id_artikel = $this->input->post('id_artikel'); 
+        $id_artikel = $this->input->post('id_artikel');
         $isi = $this->input->post('isi');
         $judul_artikel = $this->input->post('judul_artikel');
 
@@ -174,22 +174,22 @@ class Artikel extends CI_Controller {
             $_FILES['userfile']['type']= $files['userfile']['type'];
             $_FILES['userfile']['tmp_name']= $files['userfile']['tmp_name'];
             $_FILES['userfile']['error']= $files['userfile']['error'];
-            $_FILES['userfile']['size']= $files['userfile']['size']; 
+            $_FILES['userfile']['size']= $files['userfile']['size'];
 
             $this->upload->initialize($this->set_upload_options());
             if ($this->upload->do_upload()) {
-                $dataInfo = $this->upload->data();         
+                $dataInfo = $this->upload->data();
                 // DATA EDIT ARTIKEL
                 $edit_data = array(
                     'id_sekolah' => $id_sekolah,
                     'isi' => $isi,
                     'judul_artikel' => $judul_artikel,
                     'updated_by' => $id_user
-                 );
+                );
                 $where = array(
                     'id_artikel' => $id_artikel
-                 );
-                $this->DataHandle->edit('tbl_artikel', $edit_data, $where);    
+                );
+                $this->DataHandle->edit('tbl_artikel', $edit_data, $where);
 
                 // JIKA GAMBAR TIDAK ADA
                 if ($gambar_lama == "") {
@@ -197,17 +197,17 @@ class Artikel extends CI_Controller {
                     if ($dataInfo['file_size'] > 1024) {
                         $this->resize($dataInfo['file_name']);
 
-                    }        
+                    }
                     $edit_data = array(
                         'id_sekolah' => $id_sekolah,
                         'isi' => $isi,
                         'value' => $dataInfo['file_name'],
                         'judul_artikel' => $judul_artikel,
                         'updated_by' => $id_user
-                     );
+                    );
                     $where = array(
                         'id_artikel' => $id_artikel
-                     );
+                    );
                     $this->DataHandle->edit('tbl_artikel', $edit_data, $where);
                 }
                 // JIKA GAMBAR ADA
@@ -215,41 +215,41 @@ class Artikel extends CI_Controller {
                     if ($dataInfo['file_size'] > 1024) {
                         $this->resize($dataInfo['file_name']);
 
-                    }        
+                    }
                     $edit_data = array(
                         'id_sekolah' => $id_sekolah,
                         'isi' => $isi,
                         'judul_artikel' => $judul_artikel,
                         'value' => $dataInfo['file_name'],
                         'updated_by' => $id_user
-                     );
+                    );
                     $where = array(
                         'id_artikel' => $id_artikel
-                     );
+                    );
                     $this->DataHandle->edit('tbl_artikel', $edit_data, $where);
                     unlink('./assets/plugins/images/image/'.$gambar_lama);
-                }   
-                // HAPUS GAMBAR LAMA   
+                }
+                // HAPUS GAMBAR LAMA
                 $this->session->set_flashdata('msg', '
-                <div class="alert alert-success alert-dismissable">
+                    <div class="alert alert-success alert-dismissable">
                     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
                     &times;</button>
-                <i class="fa fa-check m-l-5"></i> Data Berhasil Perbaharui ... 
-                </div>');  
-                redirect('Artikel');   
+                    <i class="fa fa-check m-l-5"></i> Data Berhasil Perbaharui ...
+                    </div>');
+                redirect('Artikel');
 
             }
             else{
                 $this->session->set_flashdata('msg', '
-                <div class="alert alert-danger alert-dismissable">
+                    <div class="alert alert-danger alert-dismissable">
                     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
                     &times;</button>
                     <i class="fa fa-check m-l-5"></i> Gambar Bermasalah !!!!
-                </div>');  
+                    </div>');
 
                 redirect('Artikel');
             }
-            
+
         }
         // KONDISI GAMBAR KOSONG
         else{
@@ -259,19 +259,19 @@ class Artikel extends CI_Controller {
                 'isi' => $isi,
                 'judul_artikel' => $judul_artikel,
                 'updated_by' => $id_user
-             );
+            );
             $where = array(
                 'id_artikel' => $id_artikel
-             );
+            );
             $this->DataHandle->edit('tbl_artikel', $edit_data, $where);
             $this->session->set_flashdata('msg', '
-            <div class="alert alert-success alert-dismissable">
+                <div class="alert alert-success alert-dismissable">
                 <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
                 &times;</button>
-                <i class="fa fa-check m-l-5"></i> Data Berhasil Perbaharui ... 
-            </div>');  
-            redirect('Artikel');   
-        }   
+                <i class="fa fa-check m-l-5"></i> Data Berhasil Perbaharui ...
+                </div>');
+            redirect('Artikel');
+        }
     }
 
     private function set_upload_options(){
@@ -281,7 +281,7 @@ class Artikel extends CI_Controller {
         $config['encrypt_name'] = TRUE;
         $config['overwrite']     = FALSE;
 
-        return $config;        
+        return $config;
     }
     private function resize($nama_file_baru){
         $this->load->library('image_lib');
